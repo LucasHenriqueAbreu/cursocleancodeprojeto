9d263a8486eb96689d78da82af8fb564e88692d5
Lucas Henrique de Abreu <lucas.abreu@betrybe.com>
2022-10-17 16:46:51 -0300

    refatoração do método de verificação do CPF

--- src/before/CpfValidator.ts
+++ src/before/CpfValidator.ts
@@ -1,4 +1,9 @@
 // @ts-nocheck
+interface SplitCPf {
+  numbers: string,
+  firstCheckerDigit: string,
+  secondCheckerDigit: string,
+}
 
 export default class CpfValidator {
   public static execute(cpf: string): boolean {
@@ -7,45 +12,22 @@ export default class CpfValidator {
     }
     const cleanCpf = CpfValidator._getCleanCpf(cpf);
 
-    if (CpfValidator._allCharIsEquals(cpf)) {
+    if (CpfValidator._allCharIsEquals(cleanCpf)) {
       return false;
     }
 
-    try {
-      let d1, d2;
-      let dg1, dg2, rest;
-      let digito;
-      let nDigResult;
-      d1 = d2 = 0;
-      dg1 = dg2 = rest = 0;
-
-      for (let nCount = 1; nCount < cleanCpf.length - 1; nCount++) {
-        digito = parseInt(cleanCpf.substring(nCount - 1, nCount));
-        d1 = d1 + (11 - nCount) * digito;
-
-        d2 = d2 + (12 - nCount) * digito;
-      };
-
-      rest = (d1 % 11);
-
-      dg1 = (rest < 2) ? dg1 = 0 : 11 - rest;
-      d2 += 2 * dg1;
-      rest = (d2 % 11);
-      if (rest < 2)
-        dg2 = 0;
-      else
-        dg2 = 11 - rest;
-
-      let nDigVerific = cpf.substring(cpf.length - 2, cpf.length);
-      nDigResult = "" + dg1 + "" + dg2;
-      return nDigVerific == nDigResult;
-    } catch (e) {
-      console.error("Erro !" + e);
-
-      return false;
-    }
+    const splitCpf = CpfValidator._splitCpf(cleanCpf);
+    const firstCheckerDigit = CpfValidator._getCheckerDigit(splitCpf.numbers);
+    const secondCheckerDigit = CpfValidator._getCheckerDigit(`${splitCpf.numbers}${splitCpf.firstCheckerDigit}`);
+    return CpfValidator._compareDigits(splitCpf, firstCheckerDigit, secondCheckerDigit);
   }
 
+  static private _getCheckerDigit(numbers: string): number {
+    const multipliedNumbers = CpfValidator._multiplyDigits(numbers);
+    const sumOfMultipliedNumbers = CpfValidator._getTotalSumNumbers(multipliedNumbers);
+    const restOfDivision = sumOfMultipliedNumbers % 11;
+    return restOfDivision < 2 ? 0 : 11 - restOfDivision;
+  }
 
   static private _getCleanCpf(cpf: string): string {
     return cpf
@@ -60,7 +42,35 @@ export default class CpfValidator {
   }
 
   static private _allCharIsEquals(cpf: string): boolean {
-    const firstChar = cpf = [0];
+    const firstChar = cpf[0];
     return cpf.split('').every(c => c === firstChar);
   }
+
+  static private _splitCpf(cpf: string): SplitCPf {
+    return {
+      numbers: cpf.slice(0, cpf.length - 2),
+      secondCheckerDigit: cpf.slice(cpf.length - 1),
+      firstCheckerDigit: cpf.slice(cpf.length - 2, cpf.length - 1),
+    }
+  }
+
+  private static _multiplyDigits(digits: string): number[] {
+    const results = [];
+    for (let i = 0; i < digits.length; i++) {
+      const numberToMultiply = i + 2;
+      const indexOfDigit = (digits.length - 1) - i;
+      const digit = parseInt(digits[indexOfDigit], 10);
+      results.push(digit * numberToMultiply);
+    }
+    return results;
+  }
+
+  private static _getTotalSumNumbers(numbers: number[]): number {
+    return numbers.reduce((previousValue: number, currentValue: number) => previousValue + currentValue, 0);
+  }
+
+  private static _compareDigits(splitCpf: SplitCPf, firstCheckerDigit: number, secondCheckerDigit: number): boolean {
+    return parseInt(splitCpf.firstCheckerDigit) === firstCheckerDigit && parseInt(splitCpf.secondCheckerDigit) === secondCheckerDigit;
+  }
+
 }
